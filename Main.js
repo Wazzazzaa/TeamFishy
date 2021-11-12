c6b2aabc443f5a28fc7176c3f62d69c3ae3ed4df
wazzazzaa <tomi.eranne@gmail.com>
2021-11-12 12:52:24 +0200

    Fixes

--- Main.js
+++ Main.js
@@ -5,13 +5,23 @@ const canvasCtx = canvasElement.getContext('2d');
 function onResults(results) {
   canvasCtx.save();
   canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+  console.log(results.poseLandmarks[1].x);
   drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS,
-      {color: '#00FF00', lineWidth: 4});
+      {color: '#56e4ff', lineWidth: 4});
   drawLandmarks(canvasCtx, results.poseLandmarks,
-      {color: '#FF0000', lineWidth: 2});
+      {color: '#3364ff', lineWidth: 2});
+  /**drawConnectors(canvasCtx, results.faceLandmarks, FACEMESH_TESSELATION,
+      {color: '#56e4ff', lineWidth: 1});**/
+  drawConnectors(canvasCtx, results.leftHandLandmarks, HAND_CONNECTIONS,
+      {color: '#56e4ff', lineWidth: 5});
+  drawLandmarks(canvasCtx, results.leftHandLandmarks,
+      {color: '#3364ff', lineWidth: 2});
+  drawConnectors(canvasCtx, results.rightHandLandmarks, HAND_CONNECTIONS,
+      {color: '#56e4ff', lineWidth: 5});
+  drawLandmarks(canvasCtx, results.rightHandLandmarks,
+      {color: '#3364ff', lineWidth: 2});
   canvasCtx.restore();
 }
-
 const holistic = new Holistic({locateFile: (file) => {
     return `https://cdn.jsdelivr.net/npm/@mediapipe/holistic/${file}`;
   }});
@@ -27,7 +37,7 @@ const camera = new Camera(videoElement, {
   onFrame: async () => {
     await holistic.send({image: videoElement});
   },
-  width: 320,
-  height: 180
+  width: 100,
+  height: 100
 });
 camera.start();
